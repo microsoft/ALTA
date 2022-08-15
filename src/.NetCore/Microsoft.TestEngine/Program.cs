09f7c168296863ac2600f35581e84ca856563a67
Ruth Aramde <t-rutharamde@microsoft.com>
2022-08-15 16:49:37 -0700

    initial changes

---
+++ src/.NetCore/Microsoft.TestEngine/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.TestEngine;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Startup start = new Startup(null);
+start.ConfigureServices(builder.Services);
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/.NetCore/Microsoft.TestEngine/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.TestEngine
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationInsightsTelemetry();
+            // Configure the Connection String in appsettings.json
+        }
+
+    }
+}
