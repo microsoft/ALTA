fbed8dfcc53ac0404ae1936f44ada69bf93e8d4c
Abel Seyoum <t-abelseyoum@microsoft.com>
2022-08-18 14:09:04 -0700

    deleted redundant code

--- src/NetCore/deploymentScripts.ps1
+++ src/NetCore/deploymentScripts.ps1
@@ -3,6 +3,5 @@
 #then build alta
 #then build docker image
 
-Get-ChildItem -Path "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\Microsoft.ALTA\Microsoft.ALTA\bin\Debug\net6.0" -File | Remove-Item -Verbose
-Remove-Item "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\Microsoft.ALTA\Microsoft.ALTA\bin\Debug\net6.0" -Recurse
+Remove-Item "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\Microsoft.ALTA\Microsoft.ALTA\bin\Debug\net6.0\*" -Recurse
 Copy-Item "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\Microsoft.ALTA.SampleTest\Microsoft.ALTA.SampleTest\bin\Debug\net6.0\*" -Destination "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\Microsoft.ALTA\Microsoft.ALTA\bin\Debug\net6.0"
