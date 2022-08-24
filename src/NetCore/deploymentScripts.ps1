a898ccb962333bc3c511351a29bd2a9ba01f3080
rutharamde <41705375+rutharamde@users.noreply.github.com>
2022-08-24 10:06:55 -0700

    Delete deploymentScripts.ps1 (#12)

--- src/NetCore/deploymentScripts.ps1
+++
@@ -1,8 +0,0 @@
-#delete everything in ALTA bin folder
-#copy items from test bin into alta bin folder
-#then build alta
-#then build docker image
-
-Get-ChildItem -Path "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\Microsoft.ALTA\Microsoft.ALTA\bin\Debug\net6.0" -File | Remove-Item -Verbose
-Remove-Item "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\Microsoft.ALTA\Microsoft.ALTA\bin\Debug\net6.0" -Recurse
-Copy-Item "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\Microsoft.ALTA.SampleTest\Microsoft.ALTA.SampleTest\bin\Debug\net6.0\*" -Destination "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\Microsoft.ALTA\Microsoft.ALTA\bin\Debug\net6.0"
