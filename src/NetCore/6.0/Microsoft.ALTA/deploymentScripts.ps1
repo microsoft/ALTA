b18cd40ffffb0ade65c30da133b0edbc49cb61e5
Abel Seyoum <44584739+abel-seyoum@users.noreply.github.com>
2022-08-29 14:08:23 -0700

    Deployment Scripts (#17)
    This file allows users to zip deploy their solution as an app service to azure.

---
+++ src/NetCore/6.0/Microsoft.ALTA/deploymentScripts.ps1
@@ -0,0 +1,27 @@
+$asterisk = "\*"
+$PublishPath = "C:\Users\t-abelseyoum\source\repos\microsoft\cre-avengers-interns\ALTA\src\NetCore\6.0\Microsoft.ALTA\Microsoft.ALTA\bin\Release\net6.0\win-x64\publish"
+$SolutionPath = "C:\Users\t-abelseyoum\source\repos\microsoft\cre-avengers-interns\ALTA\src\NetCore\6.0\Microsoft.ALTA\Mircosoft.ALTA.sln"
+$TestBinariesPath = "C:\Users\t-abelseyoum\source\repos\microsoft\cre-avengers-interns\ALTA\src\NetCore\STLTest\bin\Release\net6.0\win-x64\*"
+$ZipPath = "C:\Users\t-abelseyoum\source\repos\microsoft\ALTA\src\NetCore\net6.0.zip"
+$FullPublishPath = $PublishPath+$asterisk
+$Subscription = "Cloud.Validation.and.Pipeline.Orchestration"
+$ResourceGroup = "rg-test-internship"
+$WebApp = "STLZipDeployChecker"
+
+
+$fileexists = Test-Path -Path $PublishPath
+$zipexists = Test-Path -Path $ZipPath
+
+If($zipexists){
+    Remove-Item $ZipPath -Recurse
+}
+
+If($fileexists){
+   Remove-Item $FullPublishPath -Recurse
+}
+
+dotnet publish --configuration Release --runtime win-x64 --self-contained $SolutionPath
+Copy-Item $TestBinariesPath -Destination $PublishPath -Force
+Compress-Archive -Path $FullPublishPath "net6.0.zip"
+az account set --name $Subscription
+az webapp deploy --resource-group $ResourceGroup --name $WebApp --type "zip" --src-path $ZipPath
