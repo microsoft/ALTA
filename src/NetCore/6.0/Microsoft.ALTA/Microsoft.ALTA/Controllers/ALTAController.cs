d85bcc48914cb0a6bc4ccf956e85628c9f351553
Wil Isaacs <wiisaacs@microsoft.com>
2022-09-14 09:07:25 -0700

    update to preload initializers.

--- src/NetCore/6.0/Microsoft.ALTA/Microsoft.ALTA/Controllers/ALTAController.cs
+++ src/NetCore/6.0/Microsoft.ALTA/Microsoft.ALTA/Controllers/ALTAController.cs
@@ -10,112 +10,21 @@
     [ApiController]
     public class ALTAController : Controller
     {
-        private static ConcurrentDictionary<string, Assembly> assemblies = new ConcurrentDictionary<string, Assembly>();
-        private static ConcurrentDictionary<string, object> instances = new ConcurrentDictionary<string, object>();
-        private static ConcurrentDictionary<string, Type> types = new ConcurrentDictionary<string, Type>();
-        private static ConcurrentDictionary<string, MethodInfo> methods = new ConcurrentDictionary<string, MethodInfo>();
+        //public static ConcurrentDictionary<string, Assembly> assemblies = new ConcurrentDictionary<string, Assembly>();
+        public static ConcurrentDictionary<string, object> instances = new ConcurrentDictionary<string, object>();
+        //public static ConcurrentDictionary<string, Type> types = new ConcurrentDictionary<string, Type>();
+        public static ConcurrentDictionary<string, MethodInfo> methods = new ConcurrentDictionary<string, MethodInfo>();
         private string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
         [HttpGet]
         public async Task<ActionResult> RunTest(string methodName, string assemblyName, string className, [FromQuery] string[] query = null)
         {
             object testInstance;
-            Type type;
             MethodInfo method;
-            if (methods.ContainsKey(assemblyName + className + methodName))
-            {
-                method = methods[assemblyName + className + methodName];
-                testInstance = instances[assemblyName + className];
-
-                type = types[assemblyName + className];
-
-                // running the test method
-                InitializeTest(type, testInstance);
-                await (Task)method.Invoke(testInstance, query);
-
-                return this.StatusCode(200);
-            }
-            else
-            {
-                Assembly testAssembly;
-                if (!assemblies.ContainsKey(assemblyName))
-                {
-                    testAssembly = Assembly.LoadFrom(Path.Combine(this.path, $"{assemblyName}.dll"));
-
-                    InitializeAssembly(testAssembly);
-
-                    assemblies[assemblyName] = testAssembly;
-                }
-                else
-                {
-                    testAssembly = assemblies[assemblyName];
-                }
-
-                if (types.ContainsKey(assemblyName + className))
-                {
-                    type = types[assemblyName + className];
-                }
-                else
-                {
-                    type = testAssembly.GetType(className);
-                    types[assemblyName + className] = type;
-                }
-
-                if (instances.ContainsKey(assemblyName + className))
-                {
-                    testInstance = instances[assemblyName + className];
-                }
-                else
-                {
-                    testInstance = InitializeClass(type);
-                    instances[assemblyName + className] = testInstance;
-                }
-
-                method = type.GetMethod(methodName);
-                methods[assemblyName + className + methodName] = method;
-
-                InitializeTest(type, testInstance);
-
-                await (Task)method.Invoke(testInstance, query);
-
-
-                return this.StatusCode(200);
-            }
+            method = methods[assemblyName + className + methodName];
+            testInstance = instances[assemblyName + className];
+            await (Task)method.Invoke(testInstance, query);
+            return this.StatusCode(200);
         }
-
-        private static object InitializeClass(Type t)
-        {
-            object test_Instance = Activator.CreateInstance(t);
-            var initializeClass = t.GetMethods().FirstOrDefault(x => x.GetCustomAttributes<ClassInitializeAttribute>().Any());
-
-            if (initializeClass != null)
-            {
-                initializeClass.Invoke(test_Instance, new object[] { null });
-            }
-
-            return test_Instance;
-        }
-
-
-        private static void InitializeAssembly(Assembly assembly)
-        {
-            var assemblyType = assembly.GetTypes().FirstOrDefault(y => y.IsDefined(typeof(AssemblyInitializeAttribute)));
-
-            if (assemblyType != null)
-            {
-                var initializeAssembly = assemblyType.GetMethods().FirstOrDefault(x => x.GetCustomAttributes<AssemblyInitializeAttribute>().Any());
-                initializeAssembly.Invoke(assemblyType, new object[] { null });
-            }
-        }
-
-        private static void InitializeTest(Type t, object testInstance)
-        {
-            var initializeTest = t.GetMethods().FirstOrDefault(x => x.GetCustomAttributes<TestInitializeAttribute>().Any());
-            if (initializeTest != null)
-            {
-                initializeTest.Invoke(testInstance, new object[] { null });
-            }
-        }
-
     }
 }
--- src/NetCore/6.0/Microsoft.ALTA/Microsoft.ALTA/Startup.cs
+++ src/NetCore/6.0/Microsoft.ALTA/Microsoft.ALTA/Startup.cs
@@ -1,10 +1,15 @@
-﻿namespace Microsoft.ALTA
+﻿using Microsoft.ALTA.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace Microsoft.ALTA
 {
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
             this.Configuration = configuration;
+            LoadAllAssemblies();
         }
 
         public IConfiguration Configuration { get; }
@@ -13,5 +18,89 @@
         {
             services.AddApplicationInsightsTelemetry();
         }
+
+        public void LoadAllAssemblies()
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var assemblies = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories);
+            foreach (var assemblyName in assemblies)
+            {
+                try
+                {
+                    var assembly = InitializeAssembly(Assembly.LoadFrom(assemblyName));
+                    var assemblyShortName = Path.GetFileName(assemblyName).Split(".dll")[0];
+                    var testClasses = GetTestClasses(assembly);
+                    foreach (var testClass in testClasses)
+                    {
+                        string instanceNameKey = assemblyShortName + testClass.FullName;
+                        ALTAController.instances[instanceNameKey] = InitializeClass(testClass);
+                        InitializeTest(testClass, ALTAController.instances[instanceNameKey]);
+                        RegisterMethods(testClass, ALTAController.instances[instanceNameKey], assemblyShortName);
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Failed to load assembly {assemblyName}");
+                }
+            }
+        }
+
+        public static List<Type> GetTestClasses(Assembly assembly)
+        {
+            return assembly.GetTypes().Where(x => x.IsDefined(typeof(TestClassAttribute))).ToList();
+        }
+
+        private static object InitializeClass(Type t)
+        {
+            object test_Instance = Activator.CreateInstance(t);
+            var initializeClass = t.GetMethods().FirstOrDefault(x => x.GetCustomAttributes<ClassInitializeAttribute>().Any());
+
+            if (initializeClass != null)
+            {
+                initializeClass.Invoke(test_Instance, new object[] { null });
+            }
+
+            return test_Instance;
+        }
+
+
+        private static Assembly InitializeAssembly(Assembly assembly)
+        {
+            var assemblyType = assembly.GetTypes().FirstOrDefault(y => y.IsDefined(typeof(AssemblyInitializeAttribute)));
+
+            if (assemblyType != null)
+            {
+                var initializeAssembly = assemblyType.GetMethods().FirstOrDefault(x => x.GetCustomAttributes<AssemblyInitializeAttribute>().Any());
+                initializeAssembly.Invoke(assemblyType, new object[] { null });
+            }
+            return assembly;
+        }
+
+        private static void InitializeTest(Type t, object testInstance)
+        {
+            var initializeTest = t.GetMethods().FirstOrDefault(x => x.GetCustomAttributes<TestInitializeAttribute>().Any());
+            if (initializeTest != null)
+            {
+                if(initializeTest.GetParameters().Count() > 0)
+                {
+                    initializeTest.Invoke(testInstance, new object[] { null });
+                }
+                else
+                {
+                    initializeTest.Invoke(testInstance, null);
+                }
+            }
+        }
+
+        private static void RegisterMethods(Type t, object testInstance, string assemblyName)
+        {
+            var methods = t.GetMethods().Where(m => m.GetCustomAttributes(typeof(TestMethodAttribute), false).Length > 0)
+                      .ToArray();
+            foreach(var method in methods)
+            {
+                ALTAController.methods[assemblyName + t.FullName + method.Name] = method;
+            }
+        }
+
     }
 }
